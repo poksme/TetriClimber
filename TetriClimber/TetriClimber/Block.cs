e066a421404a7740e776cfee24f86953ab262d22
Victor Sordoillet <victorsordoillet@gmail.com>
2012-12-15 14:51:15 +0100

    Added block

---
+++ TetriClimber/TetriClimber/Block.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TetriClimber
+{
+    public class Block
+    {
+        Vector2 pos = Vector2.Zero;
+
+        public Block()
+        {
+        }
+
+        public void setPosition(Vector2 coord)
+        {
+            pos = coord;
+        }
+
+        public void setPosition(int x, int y)
+        {
+            pos = new Vector2((float)x, (float)y);
+        }
+    }
+}
