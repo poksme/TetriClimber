c32010418b240a60cba79b4a37d827be48b2f7f3
Bertrand Boustany <bertrand@boustany.me>
2012-12-15 14:56:33 +0100

    ATetrimino started

---
+++ TetriClimber/TetriClimber/ATetrimino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TetriClimber
+{
+    public class ATetrimino
+    {
+        List<Action> orientations = null;
+        List<Block> shape = null;
+        int orientation;
+        SpriteManager.ESprite color;
+        Vector2 position;
+
+        public ATetrimino()
+        {
+            orientation = 0;
+            Vector2 position = new Vector2(3, -1);
+            orientations = new List<Action>();
+            orientations.Add(pos1);
+            shape = new List<Block>();
+            shape.Add(new Block());
+            shape.Add(new Block());
+            shape.Add(new Block());
+            shape.Add(new Block());
+        }
+
+        public abstract void pos1();
+    }
+}
