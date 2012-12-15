f4b0c874e06242cda8022729c6facd175d9781db
Victor Sordoillet <victorsordoillet@gmail.com>
2012-12-15 15:34:47 +0100

    TetriQ, P O

---
+++ TetriClimber/TetriClimber/TetriO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TetriClimber
+{
+    public class TetriO : ATetrimino
+    {
+        public TetriO():base()
+        {
+            color = SpriteManager.ESprite.O;
+        }
+
+        public override void  pos1()
+        {
+            shape[0].setPosition(1, 1);
+            shape[1].setPosition(1, 2);
+            shape[2].setPosition(2, 1);
+            shape[3].setPosition(2, 2);
+        }
+    }
+}
---
+++ TetriClimber/TetriClimber/TetriP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TetriClimber
+{
+    public class TetriP : ATetrimino
+    {
+        public TetriP():base()
+        {
+            color = SpriteManager.ESprite.P;
+            orientations.Add(pos2);
+            orientations.Add(pos3);
+            orientations.Add(pos4);
+        }
+
+        public override void  pos1()
+        {
+            shape[0].setPosition(0, 1);
+            shape[1].setPosition(1, 1);
+            shape[2].setPosition(2, 1);
+            shape[3].setPosition(2, 2);
+        }
+
+        public void pos2()
+        {
+            shape[0].setPosition(0, 2);
+            shape[1].setPosition(1, 0);
+            shape[2].setPosition(1, 1);
+            shape[3].setPosition(1, 2);
+        }
+
+        public void pos3()
+        {
+            shape[0].setPosition(0, 0);
+            shape[1].setPosition(0, 1);
+            shape[2].setPosition(1, 1);
+            shape[3].setPosition(2, 1);
+        }
+
+        public void pos4()
+        {
+            shape[1].setPosition(1, 0);
+            shape[2].setPosition(1, 1);
+            shape[3].setPosition(1, 2);
+            shape[0].setPosition(2, 0);
+        }
+    }
+}
