8cff0915e491cfea05f35d2c67ae2d8192fce3a4
Victor Sordoillet <victorsordoillet@gmail.com>
2012-12-15 16:14:54 +0100

    Instanced Play Scene

--- TetriClimber/TetriClimber/SceneManager.cs
+++ TetriClimber/TetriClimber/SceneManager.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace TetriClimber
 {
-    public class SceneManager
+    public class SceneManager : DrawableGameComponent
     {
         public enum EScene {ATRACT_MODE, MAIN_MENU, PLAY, PAUSE_MENU}
         Dictionary<EScene, AScene> scenes = new Dictionary<EScene, AScene>();
         private static SceneManager instance = null;
 
-        private SceneManager()
+        private SceneManager():base(App.Game)
         {
-            //scenes.add(escene.atract_mode, new atractmode());
+            scenes.add(EScene.PLAY, new Play());
         }
 
         public static SceneManager Instance
@@ -24,6 +25,8 @@ namespace TetriClimber
                     instance = new SceneManager();
                 return instance;
             }
+        
         }
+
     }
 }
