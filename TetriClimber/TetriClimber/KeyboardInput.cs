ec5e2a952dfaae4a9c8c1751b479043be2a52ee8
Bertrand Boustany <bertrand@boustany.me>
2012-12-18 12:35:44 +0100

    Started keyboard input

---
+++ TetriClimber/TetriClimber/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace TetriClimber
+{
+    public class KeyboardInput : AUserInput
+    {
+        private KeyboardState newState;
+        private KeyboardState oldState;
+
+        public KeyboardInput() : base()
+        {
+            oldState = Keyboard.GetState();
+        }
+
+        public override void begin()
+        {
+            newState = Keyboard.GetState();
+        }
+
+        public override void end()
+        {
+            oldState = newState;
+        }
+    }
+}
