ad25903524cde16cdee9d008859f5c08877acf81
Bertrand Boustany <bertrand@boustany.me>
2012-12-18 12:20:27 +0100

    Created an abstract class for input

---
+++ TetriClimber/TetriClimber/AUserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace TetriClimber
+{
+    public abstract class AUserInput
+    {
+        public enum EInput {UP, DOWN, LEFT, RIGHT, TAP}
+        Dictionary<EInput, bool> state;
+
+        public AUserInput()
+        {
+            state = new Dictionary<EInput, bool>();
+            fillState();
+        }
+
+        public bool getState(EInput e)
+        {
+            return state[e];
+        }
+
+        public abstract void begin();
+        protected abstract void fillState();
+        public abstract void end();
+    }
+}
