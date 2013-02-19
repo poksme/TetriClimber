c3862315305f662628e8267c4bf26804dd9bc69b
Bertrand Boustany <bertrand@boustany.me>
2013-02-19 17:20:13 +0100

    Tetrimino factory has a getNexTetrimino

--- TetriClimber/TetriClimber/TetriminoFactory.cs
+++ TetriClimber/TetriClimber/TetriminoFactory.cs
@@ -11,6 +11,7 @@ namespace TetriClimber
         private static TetriminoFactory instance = null;
         private Random rand = null;
         private List<Tuple<ConstructorInfo, bool>> tetriminiConstructors;
+        private int next;
 
         private TetriminoFactory()
         {
@@ -25,6 +26,7 @@ namespace TetriClimber
             tetriminiConstructors.Add(new Tuple<ConstructorInfo, bool>(typeof(TetriS).GetConstructor(tetriminoParametersType), false));
             tetriminiConstructors.Add(new Tuple<ConstructorInfo, bool>(typeof(TetriT).GetConstructor(tetriminoParametersType), false));
             tetriminiConstructors.Add(new Tuple<ConstructorInfo, bool>(typeof(TetriZ).GetConstructor(tetriminoParametersType), false));
+            next = getNextTetriminoId();
         }
 
         public static TetriminoFactory Instance
@@ -37,7 +39,25 @@ namespace TetriClimber
             }
         }
 
-        public Tuple<ATetrimino, ATetrimino> getTetrimino()
+        public Tuple<ATetrimino, ATetrimino> getTetrimino() // ALWAYS CALL THIS ONE BEFORE CALLING GET NEXT TETRIMINO
+        {
+            var ret =  new Tuple<ATetrimino,ATetrimino>((ATetrimino)tetriminiConstructors[next].Item1.Invoke(new object[] {0.7f, false}), (ATetrimino)tetriminiConstructors[next].Item1.Invoke(new object[] {0.3f, false}));
+            next = getNextTetriminoId();
+            return ret;
+        }
+
+        public ATetrimino getNextTetrimino() // RETURNS A VISUAL NEXT TETRIMINO
+        {
+            return (ATetrimino)tetriminiConstructors[next].Item1.Invoke(new object[] {1f, false});
+        }
+
+        private void resetTetriminiConstructors()
+        {
+            for (int i = 0; i < tetriminiConstructors.Count; i++)
+                tetriminiConstructors[i] = new Tuple<ConstructorInfo, bool>(tetriminiConstructors[i].Item1, false);
+        }
+
+        private int getNextTetriminoId()
         {
             int key = rand.Next(tetriminiConstructors.Count);
             int it = key;
@@ -48,17 +68,11 @@ namespace TetriClimber
                 if (it == key)
                 {
                     resetTetriminiConstructors();
-                    return getTetrimino();
+                    return getNextTetriminoId();
                 }
             }
             tetriminiConstructors[it] = new Tuple<ConstructorInfo, bool>(tetriminiConstructors[it].Item1, true);
-            return new Tuple<ATetrimino,ATetrimino>((ATetrimino)tetriminiConstructors[it].Item1.Invoke(new object[] {0.7f, false}), (ATetrimino)tetriminiConstructors[it].Item1.Invoke(new object[] {0.3f, false}));
-        }
-
-        private void resetTetriminiConstructors()
-        {
-            for (int i = 0; i < tetriminiConstructors.Count; i++)
-                tetriminiConstructors[i] = new Tuple<ConstructorInfo, bool>(tetriminiConstructors[i].Item1, false);
+            return it;
         }
     }
 }
